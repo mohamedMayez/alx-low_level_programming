facebef9cd7b05d9835bdee89c00050e9187b0da
mohamedMayez <mohamedMayez@gmail.com>
2023-04-02 22:18:43 -0700

    sec mody

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - A program that print a line with puts fanction
+ * Return 0 success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
