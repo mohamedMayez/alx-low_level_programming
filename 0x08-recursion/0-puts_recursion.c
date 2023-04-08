6049da8c6db9fc33739b4e58a759bfe6c5cab77e
mohamed mayez <mn999663@gmail.com>
2023-04-08 10:27:02 +0200

    new start

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include<main.h>
+/**
+* function - this function use putchar to print a string
+* we get the pointer string
+* Return : 0 (Suceess) 
+*/
+void _puts_recursion(char *s){
+    if (*s == '\0')
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        _putchar(*s);
+        _putchar(s+1);
+    }
+}
